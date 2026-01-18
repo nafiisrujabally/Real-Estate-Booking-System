648951571d9d6b3b4b3c65dce9b86f76f52284a4
Muhammad Nafiis Rujabally <ruja0001@algonquinlive.com>
2026-01-18 00:05:23 -0500

    Setup React + Vite + Tailwind CSS

---
+++ Client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+// import './App.css'
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Real Estate Booking System
+      </h1>
+    </div>
+  );
+}
+
+export default App;
